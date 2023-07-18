a3676778054f10c6f5bff03f246d0ad912e5229a
Mohamed Amr Refaay <mohamedamrrefaay@gmail.com>
2023-07-18 03:20:45 +0300

    add styling to the TopBar

--- src/components/Layout/Layout.styled.ts
+++ src/components/Layout/Layout.styled.ts
@@ -1,5 +1,9 @@
 import { Box, styled } from '@mui/material'
 
 export const TopBar = styled(Box)(({ theme }) => ({
-  backgroundColor: '#fff'
+  backgroundColor: '#fff',
+  padding: '20px 30px',
+  display: 'flex',
+  alignItems: 'flex-end',
+  flexDirection: 'column'
 }))
