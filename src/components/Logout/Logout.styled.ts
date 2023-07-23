28e1d5605083ddf1ab0ec6af69454ebea758b0c2
Mohamed Amr Refaay <mohamedamrrefaay@gmail.com>
2023-07-23 05:22:51 +0300

    add the initial files of Logout Component

---
+++ src/components/Logout/Logout.styled.ts
@@ -0,0 +1,3 @@
+import styled from '@emotion/styled'
+
+export const LogoutContainer = styled.button(({ theme }) => ({}))
---
+++ src/components/Logout/Logout.tsx
@@ -0,0 +1,6 @@
+import PersonIcon from '@mui/icons-material/Person'
+import { Button } from '@mui/material'
+
+export default function Logout() {
+  return <Button variant="text">Logout</Button>
+}
