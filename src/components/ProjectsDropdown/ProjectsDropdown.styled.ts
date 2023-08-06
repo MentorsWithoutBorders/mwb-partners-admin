9a27e94f53c422b23d7260554e2117ec6e1fe6af
Mohamed Refaay <69407428+Mohmed-Refaay@users.noreply.github.com>
2023-08-07 01:06:48 +0300

    Add company dropdown (#69)
    * create a custom common dropdown component
    
    * create a custom common dropdown component
    
    * add transition to the option component
    
    * add ProjectDropdown component
    
    * add switch the dropdown to text input  functionality
    
    * handle the functionlity of adding new project
    
    * add loading spinner
    
    * change the value of all projects

---
+++ src/components/ProjectsDropdown/ProjectsDropdown.styled.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled'
+import { StyledTextField } from '@/components/Input/TextField/TextField.styled'
+
+export const Container = styled.div`
+  max-width: 192px;
+`
+
+export const CreateProjectInput = styled(StyledTextField)(({ theme }) => ({
+  '& .MuiInputBase-input': {
+    margin: 0,
+    fontSize: '14px',
+    fontWeight: 500,
+    width: '100%',
+    padding: '10px',
+    borderRadius: '10px',
+    textAlign: 'left',
+    border: `1px solid ${theme.palette.doveGray}`,
+    color: theme.palette.text.primary,
+    backgroundColor: 'white',
+    position: 'relative'
+  },
+
+  marginBottom: 0
+}))
+
+export const IconContainer = styled.button`
+  all: unset;
+  position: absolute;
+  right: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`
---
+++ src/components/ProjectsDropdown/ProjectsDropdown.tsx
@@ -0,0 +1,115 @@
+import { CommonDropdown } from '@/components/Dropdown/Dropdown'
+import { StyledOption } from '@/components/Dropdown/Dropdown.styled'
+import {
+  Container,
+  CreateProjectInput,
+  IconContainer
+} from './ProjectsDropdown.styled'
+import CloseIcon from '@mui/icons-material/Close'
+import AddIcon from '@mui/icons-material/Add'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import { KeyboardEvent, useState } from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const DUMMY_DATA = [
+  {
+    id: '1',
+    name: 'Project 1'
+  },
+  {
+    id: '2',
+    name: 'Project 2'
+  },
+  {
+    id: '3',
+    name: 'Project 3'
+  }
+]
+
+export default function ProjectsDropdown() {
+  const [projects, setProjects] = useState<typeof DUMMY_DATA>(DUMMY_DATA)
+  const [selectedProject, setSelectedProject] = useState<string>('all')
+  const [createProject, setCreateProject] = useState<boolean>(false)
+  const [projectName, setProjectName] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleChange = (_: any, value: unknown) => {
+    if (value === 'create') {
+      setCreateProject(true)
+    } else {
+      setSelectedProject(value as string)
+    }
+  }
+
+  const handleCreateProject = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key !== 'Enter') return
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+      setProjects((prev) => [
+        ...prev,
+        {
+          id: Math.random().toString(),
+          name: projectName
+        }
+      ])
+
+      handleCloseCreateProject()
+    }, 2000)
+  }
+
+  const handleCloseCreateProject = () => {
+    setCreateProject(false)
+    setProjectName('')
+  }
+
+  return (
+    <Container>
+      {createProject && (
+        <Box display="flex" alignItems="center" gap={1} position={'relative'}>
+          <CreateProjectInput
+            placeholder="Project Name"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            onKeyUp={handleCreateProject}
+            disabled={isLoading}
+            autoFocus
+          />
+
+          <IconContainer
+            onClick={handleCloseCreateProject}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <CircularProgress size={20} />
+            ) : (
+              <CloseIcon color="inherit" fontSize="small" />
+            )}
+          </IconContainer>
+        </Box>
+      )}
+
+      {!createProject && (
+        <CommonDropdown value={selectedProject} onChange={handleChange}>
+          <StyledOption value="create">
+            <Box display="flex" alignItems="center" gap={0.5}>
+              <AddIcon color="inherit" fontSize="small" />
+              Create New Project
+            </Box>
+          </StyledOption>
+
+          <Divider />
+
+          <StyledOption value="all">All Projects</StyledOption>
+          {projects.map((project) => (
+            <StyledOption key={project.id} value={project.id}>
+              {project.name}
+            </StyledOption>
+          ))}
+        </CommonDropdown>
+      )}
+    </Container>
+  )
+}
