d2ff4c25803ec5e931489047012ebe01b9e325a6
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2023-08-09 23:38:13 +0700

    feat: add custom fetch api wrapper

---
+++ src/lib/api-client.ts
@@ -0,0 +1,46 @@
+// TODO: Read token from somewhere
+// const localStorageKey = '__bookshelf_token__'
+
+export async function client(
+  endpoint: string,
+  { body, ...customConfig }: { body?: object } = {}
+) {
+  const backendApiUrl = process.env.NEXT_PUBLIC_API_URL
+
+  if (!backendApiUrl) {
+    throw new Error('No backend API URL found in environment variable')
+  }
+
+  // TODO: Set token to request header
+  // const token = window.localStorage.getItem(localStorageKey)
+  // const headers = { 'content-type': 'application/json' }
+  // if (token) {
+  //   headers.Authorization = `Bearer ${token}`
+  // }
+  const config: RequestInit = {
+    method: body ? 'POST' : 'GET',
+    ...customConfig
+    // TODO: Set headers
+    // headers: {
+    //   ...headers,
+    //   ...customConfig.headers
+    // }
+  }
+  if (body) {
+    config.body = JSON.stringify(body)
+  }
+
+  const response = await window.fetch(`${backendApiUrl}/${endpoint}`, config)
+  if (response.status === 401) {
+    // TODO: Handle logout
+    // logout()
+    // window.location.assign(window.location)
+    // return
+  }
+  if (response.ok) {
+    return await response.json()
+  } else {
+    const errorMessage = await response.text()
+    return Promise.reject(new Error(errorMessage))
+  }
+}
