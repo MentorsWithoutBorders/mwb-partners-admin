e84403ad0f3e112958673fa3d6d70c1e1bcbc46c
Mohamed Amr Refaay <mohamedamrrefaay@gmail.com>
2023-09-09 23:46:15 +0300

    fix/ remove bugged code from mentors page

--- src/pages/app/mentors.tsx
+++ src/pages/app/mentors.tsx
@@ -1,7 +1,3 @@
-import Box from '@mui/material/Box'
-import Modal from '@mui/material/Modal'
-import { SxProps, Theme } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
 import * as React from 'react'
 
 import { fetchMentorAPI, fetchMentorsAPI } from '@/api/mentors'
@@ -9,41 +5,9 @@ import DataModal from '@/components/table/DataModal'
 import DataTable from '@/components/table/DataTable'
 import { Column } from '@/components/table/interfaces'
 import { DashboardLayout } from '@/containers/dashboard/DashboardLayout'
-import {
-  filterLeftMargin,
-  flexContainer
-} from '@/styles/pages/app/students.styled'
+
 import { WithAuthentication } from '@/types/with-authentication/with-authentication.type'
-
-const MentorsPage: WithAuthentication<NextPage> = () => {
-  const [searchInput, setSearchInput] = React.useState('')
-  const [searchCheckboxes, setSearchCheckboxes] = React.useState([
-    true,
-    false,
-    false,
-    false
-  ])
-
-  const handleSearchInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchInput(event.target.value)
-    // TODO: Should trigger search.
-  }
-
-  const handleSearchMenuChange = (isVisible: boolean) => {
-    // Chekc if search menu was hidden.
-    if (!isVisible) {
-      // If all values are false, then set the first one a true.
-      if (searchCheckboxes.indexOf(true) < 0) {
-        const newValues = [...searchCheckboxes]
-        newValues[0] = true
-        setSearchCheckboxes(newValues)
-      }
-
-      // TODO: Should trigger search.
-    }
-  }
+import { NextPage } from 'next'
 
 const columns: readonly Column[] = [
   { id: 'name', label: 'Name', minWidth: 170 },
@@ -76,7 +40,7 @@ const filterValues = [
   'By student organization'
 ]
 
-export default function MentorsPage() {
+const MentorsPage: WithAuthentication<NextPage> = () => {
   const [openModal, setOpenModal] = React.useState(false)
   const [dataId, setDataId] = React.useState<number>()
   const handleOpenModal = (id: number) => {
@@ -109,6 +73,6 @@ export default function MentorsPage() {
   )
 }
 
-MentorsPage.requiresAuthentication = true
+// MentorsPage.requiresAuthentication = true
 
 export default MentorsPage
