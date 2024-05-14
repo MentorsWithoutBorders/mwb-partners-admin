fab219c52e8ce6e65b643721d0fc4263be586b97
mojahidomer <mojahidomer12@gmail.com>
2024-05-14 13:57:22 +0530

    feat: added state count on dashboard

---
+++ src/lib/dashboard/dashboard-client.ts
@@ -0,0 +1,17 @@
+import { useSession } from 'next-auth/react'
+import useSWR from 'swr'
+
+export interface NgoStatsCount {
+    totalCourses:string,
+    totalHours:string,
+    totalStudents:string,
+    totalMetors:string
+}
+export interface NgoStats  {
+    ngoStats:NgoStatsCount
+}
+  export function useGetDashboardDetails(isAll:boolean) {
+    const orgId = useSession().data?.user?.organization.id
+
+    return isAll?useSWR<NgoStats>(`partners/dashboard/stats`):useSWR<NgoStats>(`partners/${orgId}/dashboard/stats`)
+  }
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -2,7 +2,6 @@ import { Box } from '@mui/material'
 import Image from 'next/image'
 import { NextPage } from 'next/types'
 import { useState } from 'react'
-
 import Button from '@/components/Button/Button'
 import DashboardItem from '@/components/DashboardItem/DashboardItem'
 import { DashboardItemsWrapper } from '@/components/DashboardItem/DashboardItem.styled'
@@ -14,26 +13,32 @@ import { DashboardLayout } from '@/containers/dashboard/DashboardLayout'
 import { DownloadCsvForm } from '@/containers/DownloadCsvForm/DownloadCsvForm'
 import { flexContainer, toggleButton } from '@/styles/pages/app/index.styled'
 import { WithAuthentication } from '@/types/with-authentication/with-authentication.type'
+import { useGetDashboardDetails,NgoStatsCount } from '@/lib/dashboard/dashboard-client'
+import LoadingPage from '@/components/LoadingPage/LoadingPage'
 
 const SAMPLE_DATA = [
   {
     title: 'Total students',
     value: '20',
+    itemName:'totalStudents',
     icon: './icons/students.svg'
   },
   {
     title: 'Total courses',
     value: '3',
+    itemName:'totalCourses',
     icon: './icons/courses.svg'
   },
   {
     title: 'Total hours',
     value: '32',
+    itemName:'totalHours',
     icon: './icons/total-hours.svg'
   },
   {
     title: 'Total mentors',
     value: '20',
+    itemName:'totalMetors',
     icon: './icons/mentors-blue.svg'
   }
 ]
@@ -126,6 +131,8 @@ const DashboardPage: WithAuthentication<NextPage> = () => {
     // TODO: Should re-trigger search.
   }
 
+  const {data,isLoading} = useGetDashboardDetails(allParticipants)
+
   return (
     <DashboardLayout title="Dashboard">
       <Box sx={{ ...flexContainer, mb: 4 }}>
@@ -143,12 +150,12 @@ const DashboardPage: WithAuthentication<NextPage> = () => {
         </Button>
       </Box>
 
-      <DashboardItemsWrapper>
-        {SAMPLE_DATA.map((item, index) => (
+      {isLoading?<LoadingPage />:(<DashboardItemsWrapper>
+        {data?.ngoStats&&SAMPLE_DATA.map((item, index) => (
           <DashboardItem
             key={index}
             title={item.title}
-            value={item.value}
+            value={data?.ngoStats[item.itemName as keyof NgoStatsCount]??0}
             icon={
               <Image
                 src={item.icon}
@@ -160,13 +167,14 @@ const DashboardPage: WithAuthentication<NextPage> = () => {
             }
           />
         ))}
-      </DashboardItemsWrapper>
+      
       <br />
       <GeoMap
         height="600px"
         popoverRenderer={GeoMapPopover}
         locations={SAMPLE_LOCATIONS}
       />
+      </DashboardItemsWrapper>)}
     </DashboardLayout>
   )
 }
