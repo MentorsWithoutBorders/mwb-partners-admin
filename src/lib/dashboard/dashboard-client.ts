748f47dac28e61a1ecd457360c73eb42dde44b44
mojahidomer <mojahidomer12@gmail.com>
2024-05-14 14:50:00 +0530

    feat: resolved es lint error issue

--- src/lib/dashboard/dashboard-client.ts
+++ src/lib/dashboard/dashboard-client.ts
@@ -2,16 +2,16 @@ import { useSession } from 'next-auth/react'
 import useSWR from 'swr'
 
 export interface NgoStatsCount {
-    totalCourses:string,
-    totalHours:string,
-    totalStudents:string,
-    totalMetors:string
+    totalCourses: string,
+    totalHours: string,
+    totalStudents: string,
+    totalMetors: string
 }
-export interface NgoStats  {
-    ngoStats:NgoStatsCount
+export interface NgoStats {
+    ngoStats: NgoStatsCount
 }
-  export function useGetDashboardDetails(isAll:boolean) {
+export function useGetDashboardDetails(isAll: boolean) {
     const orgId = useSession().data?.user?.organization.id
 
-    return isAll?useSWR<NgoStats>(`partners/dashboard/stats`):useSWR<NgoStats>(`partners/${orgId}/dashboard/stats`)
-  }
+    return isAll ? useSWR<NgoStats>(`partners/dashboard/stats`) : useSWR<NgoStats>(`partners/${orgId}/dashboard/stats`)
+}
