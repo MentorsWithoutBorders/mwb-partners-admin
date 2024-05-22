19eb82138838b8ecd70d6ba9deda78fe0aba4d59
Edmond Pruteanu <edmondpr@gmail.com>
2024-05-22 13:37:59 +0300

    Removed Download CSV button in Dashboard

--- src/containers/DownloadCsvForm/DownloadCsvForm.tsx
+++ src/containers/DownloadCsvForm/DownloadCsvForm.tsx
@@ -1,13 +1,10 @@
 import { Box, BoxProps } from '@mui/material'
 
-import Button from '@/components/Button/Button'
 import DatePicker from '@/components/Input/DateTime/DatePicker'
 import useDateRange from '@/components/Input/DateTime/useDateRange'
 
 export function DownloadCsvForm(props: BoxProps) {
   const { start, end, handleStartChange, handleEndChange } = useDateRange()
-
-  const handleDownload = () => {}
 
   return (
     <Box {...props}>
@@ -25,14 +22,6 @@ export function DownloadCsvForm(props: BoxProps) {
         onChange={handleEndChange}
         sx={{ mr: 3 }}
       />
-      <Button
-        onClick={handleDownload}
-        type="button"
-        variant="contained"
-        sx={{ fontWeight: 'normal' }}
-      >
-        Download CSV
-      </Button>
     </Box>
   )
 }
